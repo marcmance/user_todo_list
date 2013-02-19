88a5c64f51dc4aade4f6a1e9363e054af3993122
Marc Mance <mmance@signpost.com>
2013-02-19 14:16:30 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UserTodoList::Application.config.secret_token = '24bc55f51dc632cc5a72d5ba78566223f9e8a8111b1769ecab8983a74a4e17a46bf7491e3e98ed73ac88db63758bc3b138fcce1bfa18645480b7a9ad58225b62'
