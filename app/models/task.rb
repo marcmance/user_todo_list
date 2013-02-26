042b29580352f052b1529b3e36deb0f286e61da3
Marc Mance <mmance@signpost.com>
2013-02-26 14:06:45 -0500

    testing memoize

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,13 @@
 class Task < ActiveRecord::Base
+  extend ActiveSupport::Memoizable
   attr_accessible :completed, :description, :user_id
   belongs_to :user
 
   scope :completed, -> { where("completed IS true") }
   scope :not_completed, -> { where("completed IS NOT true") }
+
+  def test_mem
+    "#{user_id} #{description}"
+  end
+  memoize :test_mem
 end
